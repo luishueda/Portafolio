5c6fe4ea14797cb71ec6cb5cd240ed1cb5bb086e
luishueda <huedaluis72@gmail.com>
2025-12-29 19:52:44 -0300

    Limpieza de script de seguridad

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -4,17 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-
-# --- SCRIPT DE RESCATE (Ejecución al inicio) ---
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    # Usamos un nombre muy simple para evitar errores
-    if not User.objects.filter(username='master').exists():
-        # Crea el usuario 'master' con contraseña 'Pass1234'
-        User.objects.create_superuser('master', 'admin@test.com', 'Pass1234')
-        print(">>> ¡USUARIO 'master' CREADO CON ÉXITO! <<<")
-    else:
-        print(">>> El usuario 'master' ya existe <<<")
-except Exception as e:
-    print(f">>> ERROR CREANDO USUARIO: {e}")
