11a35a1ccabb63bf6d633355930789bc004dc0bc
luishueda <huedaluis72@gmail.com>
2025-12-29 20:11:12 -0300

    Arreglada conexion API para produccion

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// Si existe la variable de entorno (Vercel), usa esa. Si no, usa localhost (Tu PC).
+const apiUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
+
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/', // La URL de tu Django
+    baseURL: apiUrl,
 });
 
 export default api;
