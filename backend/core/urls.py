8c15bd847a059323adeeea9e5a9681511c0c1874
luishueda <huedaluis72@gmail.com>
2025-12-29 19:33:45 -0300

    Agregado script de creacion automatica de admin

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Solo para crear el usuario una vez en Render
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'tuemail@gmail.com', 'tu_password_aqui')
