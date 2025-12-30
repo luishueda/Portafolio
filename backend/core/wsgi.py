62dd482875fabf1282d01500aa689b73549f7b37
luishueda <huedaluis72@gmail.com>
2025-12-29 22:21:33 -0300

    Auto-migrate y creacion de admin

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,6 +1,21 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+# SCRIPT DE RESCATE (Ejecuta comandos al arrancar en Render)
+try:
+    print("Ejecutando migraciones...")
+    call_command('migrate')
+    
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    # Cambia 'admin_final' y 'Luis2025!' por lo que tú quieras
+    if not User.objects.filter(username='admin_final').exists():
+        User.objects.create_superuser('admin_final', 'huedaluis72@gmail.com', 'Luis2025!')
+        print("Superusuario 'admin_final' creado con éxito")
+except Exception as e:
+    print(f"Error en script de rescate: {e}")
