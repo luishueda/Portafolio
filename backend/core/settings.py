536f208a4ff9a252d28bf01b92d217d66b2ed258
luishueda <huedaluis72@gmail.com>
2025-12-29 22:12:35 -0300

    Cambiando configuracion SMTP a SSL puerto 465

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -112,12 +112,14 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # --- CONFIGURACIÓN DE EMAIL (Gmail) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
+# CAMBIO IMPORTANTE: Usar SSL y puerto 465
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False  # Apagar TLS
+EMAIL_USE_SSL = True   # Encender SSL
+
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
-
